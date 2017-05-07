38958bc711d5fdbe7b44197388e7b5f89b4b8e75
Tiago Martins <tiaguinhomartins_@hotmail.com>
2017-05-07 15:09:10 +0100

    Creating data warehouse tables

--- datawh_tables.sql
+++ datawh_tables.sql
@@ -1,5 +1,5 @@
 CREATE TABLE tempo (
-  tempo_id INTEGER NOT NULL CHECK(tempo_id>0),
+  tempo_id serial,
   hora INTEGER NOT NULL CHECK(hora>=0 and hora<24),
   dia INTEGER NOT NULL CHECK(dia>=1 and dia<=31),
   mes INTEGER NOT NULL CHECK(mes>=1 and mes<=12),
@@ -7,20 +7,20 @@ CREATE TABLE tempo (
 );
 
 CREATE TABLE taxi (
-  taxi_id INTEGER NOT NULL CHECK(taxi_id>0),
+  taxi_id serial,
   n_licenca INTEGER NOT NULL CHECK(n_licenca>0),
   PRIMARY KEY (taxi_id)
 );
 
 CREATE TABLE stand (
-  stand_id INTEGER NOT NULL CHECK(stand_id>0),
+  stand_id serial,
   nome varchar(100) NOT NULL,
   lotacao INTEGER NOT NULL CHECK(lotacao>=0),
   PRIMARY KEY (stand_id)
 );
 
 CREATE TABLE local (
-  local_id INTEGER NOT NULL CHECK(local_id>0),
+  local_id serial,
   stand_id INTEGER NOT NULL CHECK(stand_id>0),
   freguesia varchar(30) NOT NULL,
   concelho varchar(30) NOT NULL,
@@ -31,13 +31,15 @@ CREATE TABLE local (
 
 CREATE TABLE services (
   taxi_id INTEGER NOT NULL CHECK(taxi_id>0),
-  tempo_id INTEGER NOT NULL CHECK(tempo_id>0),
+  tempo_fim_id INTEGER NOT NULL CHECK(tempo_fim_id>0),
+  tempo_inicio_id INTEGER NOT NULL CHECK(tempo_inicio_id>0),
   n_viagens INTEGER NOT NULL CHECK(n_viagens>=0),
   tempo_total INTEGER NOT NULL CHECK(tempo_total>0),
-  local_id_fim INTEGER NOT NULL CHECK(local_id_fim>0),
-  local_id_inicio INTEGER NOT NULL CHECK(local_id_inicio>0),
+  local_fim_id INTEGER NOT NULL CHECK(local_fim_id>0),
+  local_inicio_id INTEGER NOT NULL CHECK(local_inicio_id>0),
   FOREIGN KEY(taxi_id) REFERENCES taxi(taxi_id),
-  FOREIGN KEY(tempo_id) REFERENCES tempo(tempo_id),
-  FOREIGN KEY(local_id_fim) REFERENCES local(local_id),
-  FOREIGN KEY(local_id_inicio) REFERENCES local(local_id)
+  FOREIGN KEY(tempo_fim_id) REFERENCES tempo(tempo_id),
+  FOREIGN KEY(tempo_inicio_id) REFERENCES tempo(tempo_id),
+  FOREIGN KEY(local_fim_id) REFERENCES local(local_id),
+  FOREIGN KEY(local_inicio_id) REFERENCES local(local_id)
 );
